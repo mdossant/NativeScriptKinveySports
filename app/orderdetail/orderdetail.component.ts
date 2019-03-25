7213c40c0df389a674cafc9815a021b6338ebcb0
mdossant <mdossant@progress.com>
2019-03-25 09:09:46 -0400

    update date from date picker

--- app/orderdetail/orderdetail.component.ts
+++ app/orderdetail/orderdetail.component.ts
@@ -11,6 +11,7 @@ import { RouterExtensions } from 'nativescript-angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { ListView } from 'tns-core-modules/ui/list-view';
 import { Page, View } from 'tns-core-modules/ui/page';
+import { DatePicker } from 'tns-core-modules/ui/date-picker';
 import * as dialog from 'tns-core-modules/ui/dialogs';
 
 @Component({
@@ -41,6 +42,8 @@ export class OrderDetailComponent implements OnInit {
     private customerDataList: ListView;
     private showingDatePicker: Boolean = false;
     private editColumn: String;
+    private index: Number;
+    private datePicker: DatePicker;
     private months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
     public constructor (private app: app, private net: net, private page: Page, private router: RouterExtensions, private screen: ActivatedRoute) {}
@@ -58,6 +61,7 @@ export class OrderDetailComponent implements OnInit {
         this.rightIcon = <View>this.page.getViewById('rightIcon');
         this.orderDataList = <ListView>this.page.getViewById('orderDataList');
         this.customerDataList = <ListView>this.page.getViewById('customerDataList');
+        this.datePicker = <DatePicker>this.page.getViewById('datePicker');
         setTimeout(() => this.getOrderDetail(),50);
     }
 
@@ -86,18 +90,29 @@ export class OrderDetailComponent implements OnInit {
         return this.months[month] + ' ' + day + ', ' + year;
     }
 
+    private getColumnLabel (name) {
+        console.log('orderdetail getColumnLabel');
+        let label = '';
+        for (let i=0; i<name.length; i++) {
+            if (i > 0 && name.charCodeAt(i) > 64 && name.charCodeAt(i) < 91) // uppercase letter?
+                label = label + ' ';
+            label = label + name.charAt(i);
+        }
+        return label;
+    }
+
     private setListData () {
         this.orderData = [];
         this.customerData = [];
-        for (let k in this.ttOrderDetail) 
+        for (let k in this.ttOrderDetail)
             if (k.indexOf('Ordernum') === -1 && k.indexOf('CustNum') === -1 && k.indexOf('SalesRep') === -1 && k.indexOf('_id') === -1)
                 if (k.indexOf('Date')>-1 && this.ttOrderDetail[k])
-                    this.orderData.push({columnLabel: k, columnValue: this.formatDate(this.ttOrderDetail[k])});
+                    this.orderData.push({columnName: k, columnLabel: this.getColumnLabel(k), columnValue: this.formatDate(this.ttOrderDetail[k])});
                 else
-                    this.orderData.push({columnLabel: k, columnValue: this.ttOrderDetail[k]});
+                    this.orderData.push({columnName: k, columnLabel: this.getColumnLabel(k), columnValue: this.ttOrderDetail[k]});
         for (let k in this.ttCustomer)
             if (k.indexOf('CustNum') === -1 && k.indexOf('SalesRep') === -1 && k.indexOf('_id') === -1)
-                this.customerData.push({columnLabel: k, columnValue: this.ttCustomer[k]});
+                this.customerData.push({columnName: k, columnLabel: this.getColumnLabel(k), columnValue: this.ttCustomer[k]});
         // trick: add some keyboard buffer area
         for (let i=0; i<6; i++) {
             this.orderData.push({});
@@ -138,33 +153,43 @@ export class OrderDetailComponent implements OnInit {
     private editOrder (index) {
         console.log('orderdetail editOrder',index);
         console.log(this.orderData[index]);
-        console.log(this.ttOrderDetail[this.orderData[index].columnLabel]);
+        console.log(this.ttOrderDetail[this.orderData[index].columnName]);
         this.orderDataList.scrollToIndexAnimated(index);
     }
 
     private editDate (index) {
         console.log('ordedetail editDate',index);
-        console.log(this.ttOrderDetail[this.orderData[index].columnLabel]);
+        console.log(this.ttOrderDetail[this.orderData[index].columnName]);
+        this.index = index;
+        let columnDate;
+        if (this.ttOrderDetail[this.orderData[index].columnName])
+            columnDate = new Date(this.ttOrderDetail[this.orderData[index].columnName]);
+        else
+            columnDate = new Date();
         this.editColumn = this.orderData[index].columnLabel;
+        this.datePicker.month = columnDate.getMonth() + 1;
+        this.datePicker.day = columnDate.getDate();
+        this.datePicker.year = columnDate.getFullYear();
         this.showingDatePicker = true;
     }
 
-    private cancelEditColumn () {
-        console.log('ordedetail cancelEditColumn');
-        this.showingDatePicker = false;;
+    private cancelEditDate () {
+        console.log('ordedetail cancelEditDate');
+        this.showingDatePicker = false;
     }
 
-    private doneEditColumn () {
-        console.log('ordedetail doneEditColumn');
-        this.showingDatePicker = false;;
+    private doneEditDate () {
+        console.log('ordedetail doneEditDate',this.datePicker.date);
+        this.updateOrder(this.index,this.datePicker.date);
+        this.showingDatePicker = false;
     }
 
     private updateOrder (index,newValue) {
         console.log('orderdetail updateOrder',index,newValue);
         console.log(this.orderData[index]);
-        console.log(this.ttOrderDetail[this.orderData[index].columnLabel]);
-        if (this.ttOrderDetail[this.orderData[index].columnLabel] !== newValue) {
-            this.ttOrderDetail[this.orderData[index].columnLabel] = newValue;
+        console.log(this.ttOrderDetail[this.orderData[index].columnName]);
+        if (this.ttOrderDetail[this.orderData[index].columnName] !== newValue) {
+            this.ttOrderDetail[this.orderData[index].columnName] = newValue;
             this.setListData();
             this.net.updateOrder({
                 ttOrder: this.ttOrderDetail,
@@ -187,16 +212,16 @@ export class OrderDetailComponent implements OnInit {
     private editCustomer (e) {
         console.log('orderdetail editCustomer',e.index);
         console.log(this.customerData[e.index]);
-        console.log(this.ttCustomer[this.customerData[e.index].columnLabel]);
+        console.log(this.ttCustomer[this.customerData[e.index].columnName]);
         this.customerDataList.scrollToIndexAnimated(e.index);
     }
 
     private updateCustomer (index,newValue) {
         console.log('orderdetail updateCustomer',index,newValue);
         console.log(this.customerData[index]);
-        console.log(this.ttCustomer[this.customerData[index].columnLabel]);
-        if (this.ttCustomer[this.customerData[index].columnLabel] !== newValue) {
-            this.ttCustomer[this.customerData[index].columnLabel] = newValue;
+        console.log(this.ttCustomer[this.customerData[index].columnName]);
+        if (this.ttCustomer[this.customerData[index].columnName] !== newValue) {
+            this.ttCustomer[this.customerData[index].columnName] = newValue;
             this.setListData();
             this.net.updateCustomer({
                 ttCustomer: this.ttCustomer,
